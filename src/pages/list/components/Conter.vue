95cac326169ba1a625f4e38224b1da7871a90017
cest <786638185@qq.com>
2019-08-26 20:29:41 +0800

    'error-fix'

---
+++ src/pages/list/components/Conter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div @click="handleClick">{{content}}</div>
+</template>
+
+<script>
+export default {
+  name: 'Conter',
+  data: function () {
+    return {
+      content: 0
+    }
+  },
+  methods: {
+    handleClick: function () {
+      this.content++
+      this.$emit('change1')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/list/components/Table.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <tr>
+      <td>{{content}}</td>
+    </tr>
+    <br>
+    <!--  ref 获取dom元素-->
+    <div ref='hello' @click="handleClick">haha</div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Table',
+  data: function () {
+    return {
+      content: 'this is a row'
+    }
+  },
+  methods: {
+    handleClick: function () {
+      alert(this.$refs.hello.innerHTML)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
